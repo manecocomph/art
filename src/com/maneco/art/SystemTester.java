b883eb8b8a02d29fff54cb45cef9d48b3260123a
manecocomph <manecocomph@163.com>
2011-09-11 22:33:40 +0800

    add some class loader example

--- src/com/maneco/art/SystemTester.java
+++ src/com/maneco/art/SystemTester.java
@@ -7,6 +7,7 @@ public class SystemTester {
 	 */
 	public static void main(String[] args) {
 		System.err.println(System.identityHashCode(null));
+		
 		Runtime.getRuntime();
 	}
 
--- src/com/maneco/art/classloader/MyClassLoader.java
+++ src/com/maneco/art/classloader/MyClassLoader.java
@@ -8,16 +8,32 @@ public class MyClassLoader extends ClassLoader {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*File f = new File("C:/Users/xiatian/Downloads/apache-cxf-2.3.3/apache-cxf-2.3.3/lib/org/apache/cxf/Bus.class");
-		System.out.println(f.exists());*/
-		
+	public static void main(String[] args) {		
 		try {
-			Class.forName("com.maneco.art.classloader");
+			Class clc = Class.forName("com.maneco.art.classloader.MyClassLoader");
+			System.out.println(clc.newInstance());
+			ClassLoader cl = clc.getClassLoader();
+			System.out.println(cl.toString());
+			
+			MyClassLoader.printAllClassLoader(clc);
 		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
 			e.printStackTrace();
 		}
 	}
-
+	
+	public static void printAllClassLoader(Object obj) {
+		if (null == obj) {
+			throw new NullPointerException("parameter can't be null");
+		}
+		Class clazz = obj.getClass();
+		ClassLoader cl = clazz.getClassLoader();
+		while (null != cl) {
+			System.out.println(cl.toString());
+			cl.getParent();
+		}
+	}
 }
