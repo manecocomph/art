26b11ce681dedef8b75bdb69b24b2ba1ad15e7d8
unknown <cqy@cqy-PC.(none)>
2011-11-26 12:33:35 +0800

    add lucene related info

---
+++ src/com/maneco/art/search/lucene/Indexer.java
@@ -0,0 +1,13 @@
+package com.maneco.art.search.lucene;
+
+public class Indexer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/maneco/art/search/mysql/Searcher.java
@@ -0,0 +1,13 @@
+package com.maneco.art.search.mysql;
+
+public class Searcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
