b2c213bc4ad89df6eedff0aba2c794e2574e52cf
manecocomph <manecocomph@163.com>
2011-12-14 12:36:08 +0800

    add mongoDB.jar

---
+++ src/com/tianxiaohui/art/mongodb/vo/Tweet.java
@@ -0,0 +1,78 @@
+package com.tianxiaohui.art.mongodb.vo;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.bson.BSONObject;
+
+import com.mongodb.DBObject;
+
+public class Tweet implements DBObject {
+
+	@Override
+	public boolean containsField(String arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsKey(String arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object get(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<String> keySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object put(String arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void putAll(BSONObject arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void putAll(Map arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object removeField(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map toMap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isPartialObject() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void markAsPartialObject() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
